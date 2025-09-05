f184a2b9786937d2bd35442aff635528cda2914d
Opeyemi <Opeyemi5055@yahoo.com>
2025-09-05 04:59:09 +0100

    task1

---
+++ prodev-mobile-app-0x05/app/join.tsx
@@ -0,0 +1,10 @@
+import {
+  Text,
+  View,
+  ImageBackground,
+  Dimensions,
+  TouchableOpacity,
+  Image,
+  StyleSheet,
+} from "react-native";
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
